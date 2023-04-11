227ce78209c0eec3f589b67a3a92ef2d31d842a4
SohibDev <sohibjonuzoqov01@gmail.com>
2023-04-12 01:08:45 +0500

    Search Functioal=nal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import "./App.css";
+import { useState } from "react";
 import countries from "./Countries";
+
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
       <h1 className="text-center bg-success pt-3 pb-3">
@@ -12,25 +23,32 @@ function App() {
           className="form-control w-50 mx-auto bg-info mb-5"
           id="country"
           placeholder="Enter country(s) name! ✍️"
-          />
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
         <div className="error-message"></div>
         <div className="d-flex justify-content-around flex-wrap">
-        {countries.map((country) => {
-          return (
-            <>
-                <div class="card w-25 p-3 m-2">
-                  <img src={country.flags.png} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">{country.name.common}</h5>
-                    <p class="card-text">{country.translations.tur.official}</p>
-                    <a href={country.maps.googleMaps} class="btn btn-primary">
-                      Tap to see Map
-                    </a>
-                  </div>
+          {filteredCountries.map((country, index) => {
+            return (
+              <div key={index} className="card w-25 p-3 m-2">
+                <img
+                  src={country.flags.png}
+                  className="card-img-top"
+                  alt="..."
+                />
+
+                <div className="card-body">
+                  <h5 className="card-title">{country.name.common}</h5>
+                  <p className="card-text">
+                    {country.translations.tur.official}
+                  </p>
+                  <a href={country.maps.googleMaps} className="btn btn-primary">
+                    Tap to see Map
+                  </a>
                 </div>
-            </>
-          );
-        })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
