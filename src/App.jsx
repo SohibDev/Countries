ad5d75727cc28d93b865fdea3b42b44782e0f670
SohibDev <sohibjonuzoqov01@gmail.com>
2023-05-02 20:45:20 +0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -18,33 +18,37 @@ function App() {
         Enter your country name! 🌍
       </h1>
       <div className="container p-3">
-        <input
-          type="search"
-          className="form-control w-50 mx-auto bg-info mb-5"
-          id="country"
-          placeholder="Enter country(s) name! ✍️"
-          value={searchQuery}
-          onChange={handleSearchInputChange}
-        />
-        <div className="error-message"></div>
-        <div className="d-flex justify-content-around flex-wrap">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <input
+              type="search"
+              className="form-control bg-info mb-5"
+              id="country"
+              placeholder="Enter country(s) name! ✍️"
+              value={searchQuery}
+              onChange={handleSearchInputChange}
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center">
           {filteredCountries.map((country, index) => {
             return (
-              <div key={index} className="card w-25 p-3 m-2">
-                <img
-                  src={country.flags.png}
-                  className="card-img-top"
-                  alt="..."
-                />
-
-                <div className="card-body">
-                  <h5 className="card-title">{country.name.common}</h5>
-                  <p className="card-text">
-                    {country.translations.tur.official}
-                  </p>
-                  <a href={country.maps.googleMaps} className="btn btn-primary">
-                    Tap to see Map
-                  </a>
+              <div key={index} className="col-md-4 mb-4">
+                <div className="card p-3">
+                  <img
+                    src={country.flags.png}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{country.name.common}</h5>
+                    <p className="card-text">
+                      {country.translations.tur.official}
+                    </p>
+                    <a href={country.maps.googleMaps} className="btn btn-primary">
+                      Tap to see Map
+                    </a>
+                  </div>
                 </div>
               </div>
             );
