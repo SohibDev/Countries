180ecb07eb2339da51f03a346561d34a23c3b87f
SohibDev <sohibjonuzoqov01@gmail.com>
2023-05-02 21:04:36 +0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import countries from "./Countries";
+import "./App.css";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -18,34 +19,37 @@ function App() {
         Enter your country name! 🌍
       </h1>
       <div className="container p-3">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <input
-              type="search"
-              className="form-control bg-info mb-5"
-              id="country"
-              placeholder="Enter country(s) name! ✍️"
-              value={searchQuery}
-              onChange={handleSearchInputChange}
-            />
-          </div>
-        </div>
+        <input
+          type="search"
+          className="form-control w-50 mx-auto bg-info mb-5"
+          id="country"
+          placeholder="Enter country(s) name! ✍️"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
+        <div className="error-message"></div>
         <div className="row justify-content-center">
           {filteredCountries.map((country, index) => {
             return (
-              <div key={index} className="col-md-4 mb-4">
-                <div className="card p-3">
+              <div key={index} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div className="card h-100 shadow-sm">
                   <img
                     src={country.flags.png}
                     className="card-img-top"
                     alt="..."
                   />
+
                   <div className="card-body">
-                    <h5 className="card-title">{country.name.common}</h5>
-                    <p className="card-text">
+                    <h5 className="card-title text-truncate">
+                      {country.name.common}
+                    </h5>
+                    <p className="card-text text-truncate">
                       {country.translations.tur.official}
                     </p>
-                    <a href={country.maps.googleMaps} className="btn btn-primary">
+                    <a
+                      href={country.maps.googleMaps}
+                      className="btn btn-primary"
+                    >
                       Tap to see Map
                     </a>
                   </div>
